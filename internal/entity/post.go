a06b206989e5a37f862103c07faf0419db23e2f7
arief romadhon <ariefromadhon26@gmail.com>
2025-01-15 11:43:30 +0700

    add post entity

---
+++ internal/entity/post.go
@@ -0,0 +1,13 @@
+package entity
+
+import "time"
+
+type Post struct {
+	ID        int64     `json:"id"`
+	Title     string    `json:"title"`
+	Content   string    `json:"content"`
+	UserID    int64     `json:"user_id"`
+	Tags      []string  `json:"tags"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"Updated_at"`
+}
