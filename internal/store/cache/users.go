4c3cffcfc059cafcff9fdb2a2497d10487df93aa
arief romadhon <ariefromadhon26@gmail.com>
2025-01-28 15:51:03 +0700

    feat:add caching the user profile

--- internal/store/cache/users.go
+++ internal/store/cache/users.go
@@ -1,6 +1,12 @@
 package cache
 
 import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"time"
+
+	"github.com/ariefro/threads-server/internal/store"
 	"github.com/redis/go-redis/v9"
 )
 
@@ -14,4 +20,38 @@ type userStore struct {
 	rdb *redis.Client
 }
 
-type UserStorage interface{}
+type UserStorage interface {
+	Get(context.Context, int64) (*store.User, error)
+	Set(context.Context, *store.User) error
+}
+
+const UserExpTime = time.Minute
+
+func (s *userStore) Get(ctx context.Context, userID int64) (*store.User, error) {
+	cacheKey := fmt.Sprintf("user-%d", userID)
+	data, err := s.rdb.Get(ctx, cacheKey).Result()
+	if err == redis.Nil {
+		return nil, nil
+	} else if err != nil {
+		return nil, err
+	}
+	var user store.User
+	if data != "" {
+		err := json.Unmarshal([]byte(data), &user)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return &user, nil
+}
+
+func (s *userStore) Set(ctx context.Context, user *store.User) error {
+	cacheKey := fmt.Sprintf("user-%d", user.ID)
+
+	json, err := json.Marshal(user)
+	if err != nil {
+		return err
+	}
+
+	return s.rdb.SetEx(ctx, cacheKey, json, UserExpTime).Err()
+}
